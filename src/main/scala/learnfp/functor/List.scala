e2734c6d898eb731704ccc28a3f91b51078fe9f8
Jose Andres Villareal <jvillarreal@elementanalytics.com>
2023-05-16 10:08:59 -0500

    Solution from authors to List Functor typeclass

--- src/main/scala/learnfp/functor/List.scala
+++ src/main/scala/learnfp/functor/List.scala
@@ -2,9 +2,6 @@ package learnfp.functor
 
 object ListInstance {
   implicit val listInstance:Functor[List] = new Functor[List] {
-    override def fmap[A, B](a: List[A])(fx: A => B): List[B] = a match {
-      case Nil => List.empty[B]
-      case head :: tl => fx(head) +: fmap(tl)(fx)
-    }
+    override def fmap[A, B](a: List[A])(fx: A => B): List[B] = a map fx
   }
 }
