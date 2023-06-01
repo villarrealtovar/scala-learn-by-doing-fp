0a5b306d6d02567b2d759c025db378921a35bf10
Jose Andres Villareal <jvillarreal@elementanalytics.com>
2023-06-01 09:23:33 -0500

    Author Solution for monads/maybe

--- src/main/scala/learnfp/monad/Maybe.scala
+++ src/main/scala/learnfp/monad/Maybe.scala
@@ -5,10 +5,10 @@ import learnfp.functor.MaybeInstance._
 
 object MaybeInstance {
   implicit val monadInstance = new Monad[Maybe] {
-    override def pure[A](a: A): Maybe[A] = Just(a)
+    override def pure[A](a: A): Maybe[A] = just(a)
     override def flatMap[A, B](a: Maybe[A])(fx: A => Maybe[B]): Maybe[B] = a match {
       case Just(value) => fx(value)
-      case Nothing() => Nothing[B]
+      case Nothing() => nothing[B]()
     }
   }
 }
